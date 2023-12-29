244263242d202a2a9e879680c0029e5f219fb99c
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2023-12-29 06:38:16 +0300

    feat:add getRequest service
    Add getRequest service to perform a get request to the server

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -22,3 +22,23 @@ export const postRequest = async(url:string,body:RegisterInfoState|LoginInfoStat
     }
     return data;
 }
+export const getRequest = async(url:string)=>{
+    const headers = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const response = await axios.get(url,headers);
+    const data = await response.data;
+    if (response.status === 400||response.status === 500) {
+        let message
+        if (data?.message) {
+            message = data.message;
+            
+        }else{
+            message = data;
+        }
+        return {error:true,message};
+    }
+    return data;
+}
