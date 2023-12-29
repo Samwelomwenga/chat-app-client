a19294c02a92d09f45709820cdb90646c39d2d91
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2023-12-29 13:49:29 +0300

    feat: add fetchChatsReducer
    Add fetchChatsReducer to handle fetching chats from the server

---
+++ src/utils/functions/fetchChatsReducer.ts
@@ -0,0 +1,45 @@
+import { FetchChatsInitialState } from "../hooks/useFetch";
+
+ export type FetchChatsAction =
+|{
+    type:"FETCH_CHATS_REQUEST"
+}
+|{
+    type:"FETCH_CHATS_SUCCESS"
+    payload:string[]
+}
+|{
+    type:"FETCH_CHATS_FAIL"
+    payload:{message:string,isError:boolean}
+}
+
+const fetchChatsReducer = (state:FetchChatsInitialState, action:FetchChatsAction) => {
+  switch (action.type) {
+    case "FETCH_CHATS_REQUEST": {
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    }
+    case "FETCH_CHATS_SUCCESS": {
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        userChats: action.payload,
+      };
+    }
+    case "FETCH_CHATS_FAIL": {
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    }
+   
+    default:
+      return state;
+  }
+}
+export default fetchChatsReducer;
