e9ee6c231f010cdc1e532dbb588c99f9acaab510
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-02 10:34:04 +0300

    feat: add potentialChatUsersReducer
    Add potentialChatUsersReducer to handle the state of the potential chat
    users

---
+++ src/utils/functions/potentialChatUsersReducer.ts
@@ -0,0 +1,48 @@
+import { PotentialChatUsersInitialState } from "../../hooks/useFetchPotentialChatUsers"
+// import { RecipientDetails as Users } from "../../hooks/useFetchRecipient"
+import { Users } from "../../hooks/useFetchPotentialChatUsers"
+
+export type PotentialChatUsersAction =
+|{
+   type:"FETCH_CHATS_REQUEST"
+}
+|{
+   type:"FETCH_CHATS_SUCCESS"
+   payload:Users
+}
+|{
+   type:"FETCH_CHATS_FAIL"
+   payload:{message:string,isError:boolean}
+}
+
+const potentialChatUsersReducer = (state:PotentialChatUsersInitialState, action:PotentialChatUsersAction) => {
+ switch (action.type) {
+   case "FETCH_CHATS_REQUEST": {
+     return {
+       ...state,
+       loading: true,
+       error: null,
+     };
+   }
+   case "FETCH_CHATS_SUCCESS": {
+     return {
+       ...state,
+       loading: false,
+       error: null,
+       potentialChatUsers: action.payload,
+     };
+   }
+   case "FETCH_CHATS_FAIL": {
+     return {
+       ...state,
+       loading: false,
+       error: action.payload,
+     };
+   }
+   
+   default:
+     return state;
+ }
+}
+
+export default potentialChatUsersReducer;
