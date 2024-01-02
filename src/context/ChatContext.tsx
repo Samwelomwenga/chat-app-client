598c0358ff83f21d8afff81fe1660497d3faeaad
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-02 10:38:42 +0300

    feat: add potentialChatUsersState
    Add potentialChatUsersState to ChatContext

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,15 +1,21 @@
 import { createContext,ReactNode } from "react";
 import { UserPayload as User } from "./AuthContext";
-import useFetch, { fetchChatsInitialState, FetchChatsInitialState } from "../hooks/useFetch";
+import useFetch, { fetchChatsInitialState as fetchChatsState, FetchChatsInitialState } from "../hooks/useFetch";
+import useFetchPotentialChatUsers, { PotentialChatUsersInitialState ,potentialChatUsersInitialState as potentialChatUsersState} from "../hooks/useFetchPotentialChatUsers";
 
 type ChatContextProps = {
     children: ReactNode;
     user:User;
     };
+type ChatContextState = {
+    fetchChatsState:FetchChatsInitialState;
+    potentialChatUsersState:PotentialChatUsersInitialState;
+    };
 
-export const ChatContext = createContext<FetchChatsInitialState>(fetchChatsInitialState);
+export const ChatContext = createContext<ChatContextState>({fetchChatsState,potentialChatUsersState});
 
 export const ChatContextProvider = ({ children,user}:ChatContextProps) => {
     const fetchChatsState=useFetch(user)
-  return <ChatContext.Provider value={fetchChatsState}>{children}</ChatContext.Provider>;
+const potentialChatUsersState=useFetchPotentialChatUsers(fetchChatsState.userChats,user);
+  return <ChatContext.Provider value={{fetchChatsState,potentialChatUsersState}}>{children}</ChatContext.Provider>;
 };
