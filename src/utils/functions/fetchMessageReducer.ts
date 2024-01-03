a37b6fabb4312b1bc0a61b8b5c4433b50bec7552
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-03 13:44:02 +0300

    feat: add fetchMessageReducer
    Add fetchMessageReducer to handle fetching messages from the server

---
+++ src/utils/functions/fetchMessageReducer.ts
@@ -0,0 +1,42 @@
+import { MessageInitialState } from "../../hooks/useFetchMessages"
+import { Message } from "../../hooks/useFetchMessages"
+type FetchMessageActions={
+    type:"FETCH_MESSAGE_REQUEST"
+}
+|{
+    type:"FETCH_MESSAGE_SUCCESS"
+    payload:Message[]
+}
+|{
+    type:"FETCH_MESSAGE_FAIL"
+    payload:{message:string,isError:boolean}
+}
+
+
+const fetchMessageReducer=(state:MessageInitialState,action:FetchMessageActions)=>{
+    switch(action.type){
+        case 'FETCH_MESSAGE_REQUEST':
+            return{
+                ...state,
+                loading:true,
+                error:{message:"",isError:false}
+            }
+        case 'FETCH_MESSAGE_SUCCESS':
+            return{
+                ...state,
+                loading:false,
+                error:{message:"",isError:false},
+                message:action.payload
+            }
+        case 'FETCH_MESSAGE_FAIL':
+            return{
+                ...state,
+                loading:false,
+                error:{message:action.payload.message,isError:true}
+            }
+        default:
+            return state
+    }
+
+}
+export default fetchMessageReducer;
