1262ca7a97e6e6c4834250693a170d34e4ec4db2
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-08 18:42:27 +0300

    feat: add onlineUsers event listener
    Add onlineUsers event listener to ChatContext.tsx to update the
    onlineUsers state when a user logs in or out

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { UserPayload as User } from "./AuthContext";
 import useFetch, {
   fetchChatsInitialState as fetchChatsState,
@@ -14,6 +14,8 @@ import useFetchMessages, {
 } from "../hooks/useFetchMessages";
 import { FetchMessageActions } from "../utils/functions/fetchMessageReducer";
 
+import io, { Socket } from "socket.io-client";
+
 type ChatContextProps = {
   children: ReactNode;
   user: User;
@@ -25,21 +27,26 @@ type ChatContextState = {
   dispatchMessages: React.Dispatch<FetchMessageActions>;
   updateCurrentChat: (chat: Chat) => void;
   currentChat: Chat;
+  onlineUsers: OnlineUser[];
 };
 
 export const ChatContext = createContext<ChatContextState>({
   fetchChatsState,
   potentialChatUsersState,
   currentChat: { _id: "", members: [], createdAt: "", updatedAt: "" },
-  dispatchMessages:()=>{},
-  messagesState:{
-    messages:{messages:[]},
-    loading:false,
-    error:{message:"",isError:false}
-
+  dispatchMessages: () => {},
+  messagesState: {
+    messages: { messages: [] },
+    loading: false,
+    error: { message: "", isError: false },
   },
   updateCurrentChat: () => {},
+  onlineUsers: [],
 });
+type OnlineUser = {
+  userId: string;
+  socketId: string;
+};
 
 export const ChatContextProvider = ({ children, user }: ChatContextProps) => {
   const [currentChat, setCurrentChat] = useState<Chat>({
@@ -51,13 +58,35 @@ export const ChatContextProvider = ({ children, user }: ChatContextProps) => {
   const updateCurrentChat = (chat: Chat) => {
     setCurrentChat(chat);
   };
-  const {messagesState,dispatchMessages} = useFetchMessages(currentChat);
+  const { messagesState, dispatchMessages } = useFetchMessages(currentChat);
   const fetchChatsState = useFetch(user);
   const potentialChatUsersState = useFetchPotentialChatUsers(
     fetchChatsState.userChats,
     user
   );
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [onlineUsers, setOnlineUsers] = useState<OnlineUser[]>([]);
+  console.log("onlineUsers", onlineUsers);
 
+  useEffect(() => {
+    const newSocket = io("http://localhost:5000");
+    setSocket(newSocket);
+    newSocket.on("connect", () => {
+      console.log("connected to socket", newSocket.id);
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [user]);
+  useEffect(() => {
+    if (socket) {
+      socket.emit("join", user.id);
+      socket.on("getOnlineUsers", (onlineUsers: OnlineUser[]) => {
+        setOnlineUsers(onlineUsers);
+      });
+    }
+  }, [user.id, socket]);
   return (
     <ChatContext.Provider
       value={{
@@ -67,6 +96,7 @@ export const ChatContextProvider = ({ children, user }: ChatContextProps) => {
         messagesState,
         dispatchMessages,
         currentChat,
+        onlineUsers,
       }}
     >
       {children}
