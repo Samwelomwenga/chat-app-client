16aa8ebbb5a2f251eeae7b377948bd1e1bba5ee3
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-12 13:44:01 +0300

    fix: change the Chatbox design
    Change the Chatbox design to make it more user friendly

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,9 +1,10 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardContent, Typography } from "@mui/material";
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { AuthContext } from "../context/AuthContext";
 import useFetchRecipient from "../hooks/useFetchRecipient";
 import moment from "moment";
+import stringAvatar from "../utils/functions/stringAvatar";
 
 const ChatBox = () => {
   const authContext = useContext(AuthContext);
@@ -18,25 +19,38 @@ const ChatBox = () => {
   };
 
   const recipientState = useFetchRecipient(currentChat, user);
-  console.log("messages", messages);
+  const { recipient, loading } = recipientState;
 
   return (
-    <Box sx={{ bgcolor: "grey", pt: "1rem" }}>
-      <Typography variant="h6" sx={{ textAlign: "center" }}>
-        {recipientState.recipient?.user.name}
+    <Box sx={{ pt: "1rem" }}>
+      <Typography variant="h6" sx={{ textAlign: "center" ,fontWeight:"600"}}>
+        {recipient?.user.name}
       </Typography>
-      {recipientState.loading && <Typography>Loading...</Typography>}
-      {!recipientState.recipient && (
-        <Typography>No conversation selected yet...</Typography>
-      )}
-      {messages.messages.map((message, index) => (
-        <Card key={index} sx={{ width: "75%", mb: "2rem" }}>
-          <CardContent>
-            <Typography>{message.text}</Typography>
-            <Typography>{moment(message.createdAt).calendar()}</Typography>
-          </CardContent>
-        </Card>
-      ))}
+      {loading && <Typography>Loading...</Typography>}
+      {!recipient && <Typography>No conversation selected yet...</Typography>}
+      {messages.messages.map((message, index) => {
+        const isRecipient = recipient?.user._id === message.senderId;
+
+        return (
+          <Box sx={{display:"flex",alignItems:"center",justifyContent:isRecipient?"flex-start":"flex-end"}}>
+          {isRecipient&&<Avatar {...stringAvatar(recipient?.user.name|| "Anonymous User")} />}
+
+          <Card
+            key={index}
+            sx={{
+              width: "70%",
+              mb: "2rem",
+              bgcolor: !isRecipient ? "lightgreen" : "primary.main",
+            }}
+          >
+            <CardContent>
+              <Typography>{message.text}</Typography>
+              <Typography>{moment(message.createdAt).calendar()}</Typography>
+            </CardContent>
+          </Card>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
